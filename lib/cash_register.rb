1ebfa4d921749c5b6ea65264904fec91182f13b4
Irina Kagan <irinagamb@gmail.com>
2019-05-07 00:26:21 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,9 +10,9 @@ class CashRegister
 
 
   def add_item(title, price, quantity = 1)
-    if quantity>1
-      i=0
-      while i<quantity
+    if quantity > 1
+      i = 0
+      while i < quantity
         @items << title
         i+=1
       end
